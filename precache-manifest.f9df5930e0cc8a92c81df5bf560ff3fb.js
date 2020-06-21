ef59c284f8165b24c77ac1b237eba1859e2d2144
Digpal Singh Rana <digplrana35@gmail.com>
2020-06-21 19:22:57 +0530

    Updates

---
+++ precache-manifest.f9df5930e0cc8a92c81df5bf560ff3fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2134d61f3b7b944840d0197e577bcf2",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "c3896cbda5589160f552",
+    "url": "/seasons/static/css/main.a6b46de1.chunk.css"
+  },
+  {
+    "revision": "bac892c45e5ea11d1c30",
+    "url": "/seasons/static/js/2.a9736049.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/seasons/static/js/2.a9736049.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3896cbda5589160f552",
+    "url": "/seasons/static/js/main.1d3ac582.chunk.js"
+  },
+  {
+    "revision": "c60f3900773455078f75",
+    "url": "/seasons/static/js/runtime-main.f6daaeb9.js"
+  }
+]);
